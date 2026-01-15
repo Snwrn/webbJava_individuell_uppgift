56f53ebcdd73c3aca8534f17616f7b29ea782701
snwrn <kretiv.ksnwrn@gmail.com>
2026-01-15 12:14:21 +0100

    User controller added

--- src/main/java/webb_kurs/individuell_uppgift/user/UserController.java
+++ src/main/java/webb_kurs/individuell_uppgift/user/UserController.java
@@ -1,4 +1,24 @@
 package webb_kurs.individuell_uppgift.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody CreateUserRequest request) {
+        var user = userService.createUser(request);
+        return ResponseEntity.created(URI.create("/user")).build();
+    }
 }
